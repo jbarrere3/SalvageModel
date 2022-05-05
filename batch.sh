67cb5982c8f2c3f1ae18e3b885cd949ba970644e
Julien Barrere <julienbarrere3@gmail.com>
2022-05-05 09:11:07 +0200

    Adapted the batch script to the new R version

--- batch.sh
+++ batch.sh
@@ -5,6 +5,6 @@
 #SBATCH --mem-per-cpu=8000
 #SBATCH --partition=small
 
-module load r-env-singularity/4.0.5
+module load r-env-singularity/4.1.1
 
 srun singularity_wrapper exec Rscript --no-save cmd.R
